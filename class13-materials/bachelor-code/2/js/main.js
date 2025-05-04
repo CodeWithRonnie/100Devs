aace897743ae74b5745cfa315fcb1be77846579e
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-05-04 18:10:35 +0200

    Played with some more JavaScript on the barchelor-code exercise

--- class13-materials/bachelor-code/2/js/main.js
+++ class13-materials/bachelor-code/2/js/main.js
@@ -2,24 +2,29 @@ const andi = document.querySelector('#andi')
 const claire = document.querySelector('#claire')
 const sharleen = document.querySelector('#sharleen')
 
-document.querySelector('#andiNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#claireNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#sharleenNext').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#andiNext').addEventListener('click', andiNext)
+document.querySelector('#claireNext').addEventListener('click', claireNext)
+document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
 
 function andiNext(){
-	claire.classList.add(/*INSERTCODE*/)
-	sharleen.classList.add(/*INSERTCODE*/)
-	andi.classList.toggle(/*INSERTCODE*/)
+	claire.classList.add('hidden')
+	sharleen.classList.add('hidden')
+	andi.classList.toggle('hidden')
 }
 
 function claireNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+	andi.classList.add('hidden')
+	sharleen.classList.add('hidden')
+	claire.classList.toggle('hidden')
+	//toggle means to simply switch on and off.
+	//when i click on andi the image will show and when i click andi again it will be hidden.
+	
 }
 
 function sharleenNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+	claire.classList.add('hidden')
+	andi.classList.add('hidden')
+	sharleen.classList.toggle('hidden')
+	//we dont add the . for the class because the .ClassList already tells us that this is a class
+	
 }
